0214a1ad60ca3b4349f5b298d3427fbae7860cb2
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-08-22 15:16:56 +0300

    deploying

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 
-import React, { useCallback, useEffect, useState } from "react"
+import React, {  useEffect, useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -601,7 +601,7 @@ useEffect(() => {
       }
     };
 
-    const handleRefreshRequest = (event: CustomEvent) => {
+    const handleRefreshRequest = () => {
       refreshWishlistData();
     };
 
