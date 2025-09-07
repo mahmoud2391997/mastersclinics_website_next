d8c5e41f903c5cad5fd46a5b3070133753318319
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-09-08 01:08:50 +0300

    logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-
 import SocialMedia from '@/helpers/main-component/whatsApp'
 import { Html, Head, Main, NextScript } from 'next/document'
-// import { useState } from 'react';
 
 export default function Document() {
-   
   return (
-    <Html lang="ar" className='w-screen' dir="rtl">
-      <Head />
+    <Html lang="ar" dir="rtl" className="w-screen">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Masters Clinics</title>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="https://cdn.salla.sa/cdn-cgi/image/fit=scale-down,width=400,height=400,onerror=redirect,format=auto/dEYvd/lBmMUm3zZyt94KtrsYYdL6UrUEOoncu4UJnK9VhR.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
